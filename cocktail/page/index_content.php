7a642a9db0ebcf73e39d4f6e0a73792315837008
Steel <alexandre0312@orange.fr>
2023-06-12 14:30:49 +0200

    index folder assets

--- cocktail/page/index_content.php
+++ cocktail/page/index_content.php
@@ -6,25 +6,31 @@ $query = "SELECT * FROM Ingredients";
 $stmt = $dbh->query($query);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Affichage des résultats
 if (count($results) > 0) {
-    echo '<div class="table-responsive">';
-    echo '<table class="table table-striped">';
-    echo '<thead class="thead-dark">';
-    echo '<tr><th>ID</th><th>Ingrédients</th></tr>';
-    echo '</thead>';
-    echo '<tbody>';
+    ?>
+    <div class="table-responsive">
+    <table class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+            <th></th>
+            <th>Libellé</th>
+            <th>Image</th></tr>
+    </thead>
+    <tbody>
+        <?php
 
     foreach ($results as $row) {
         echo '<tr>';
-        echo '<td>' . $row['id'] . '</td>';
+        echo '<td><input type="checkbox" name="checkbox[]" value="' . $row['id'] . '"></td>';
         echo '<td>' . $row['Libelle'] . '</td>';
+        echo '<td><img src="' . $row['image_id'] . '" alt="Image"></td>';
         echo '</tr>';
     }
-
-    echo '</tbody>';
-    echo '</table>';
-    echo '</div>';
+?>
+    </tbody>
+    </table>
+    </div>
+    <?php
 } else {
     echo 'Aucun résultat trouvé.';
 }
