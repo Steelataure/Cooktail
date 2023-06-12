e39ca944a199cbdc1e3ecc5814989b4adc54abcd
alexis fredriksen <alexis-fredriksen@semper-co.fr>
2023-06-12 02:10:43 -0700

    connexion terminer presque

--- config/connection.php
+++ config/connection.php
@@ -1,38 +1,22 @@
 <?php 
 
 if(isset($_POST['connexion'])){
-    $email = $_POST['email'];
-    $motDePasse = $_POST['motDePasse'];
-    $sql = "SELECT * FROM utilisateur WHERE email = :email";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM user WHERE username = :username";
     $query = $dbh->prepare($sql);
-    $query->bindParam(":email", $email, PDO::PARAM_STR);
+    $query->bindParam(":username", $username, PDO::PARAM_STR);
     $query->execute();
     $data = $query->fetch(PDO::FETCH_OBJ);
           
-    if(isset($data->email) && password_verify($motDePasse, $data->motDePasse)){
-        $_SESSION['isLogged'] = $email;
-    
-        $utilisateur_id  = getUtilisateurId();
-        $TypeDeCompte = getTypeAccount();
+    if(isset($data->username) && password_verify($password, $data->password)){
+        $_SESSION['isLogged'] = $username;
 
-        if ($TypeDeCompte == "entreprise"){
-            if(getProfilEntreprise()){
-                header('Location: ' . $routes['swipePageEntreprise']);
-                exit;
-            }else{
-                header('Location: ' . $routes['home']);
-                exit;
-            }
-        } 
-        if ($TypeDeCompte == "candidat"){
-           if(getProfilCandidat()){
-                header('Location: ' . $routes['swipePageCandidat']);
-                exit;
-            }else{
-                header('Location: ' . $routes['home']);
-                exit;
-            }
-        }
+        echo "<div><h3 class='message'>Vous etes connecté</3></div>";
+    
+     
+
+  
     }else{
         echo "<div><h3 class='message'>Email ou mot de passe incorrect</3></div>";
     }
