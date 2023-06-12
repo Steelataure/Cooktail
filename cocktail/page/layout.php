04bb6a8675dcf7e1f5b747c1244799bf24ce0761
Steel <alexandre0312@orange.fr>
2023-06-12 14:03:14 +0200

    update navbar

--- cocktail/page/layout.php
+++ cocktail/page/layout.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Mon site</title>
+    <title>My Cocktail</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
@@ -12,7 +12,7 @@
 <body>
     <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">Navbar</a>
+  <a class="navbar-brand" href="index.php">My Cocktail</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
