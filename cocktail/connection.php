7ab3b2bbc59236d7fa26f7cea21ff62ea34fe27c
Steel <alexandre0312@orange.fr>
2023-06-12 16:14:00 +0200

    update login

--- cocktail/connection.php
+++ cocktail/connection.php
@@ -1,20 +1,39 @@
  <?php include "../config/connection.php" ?>
 
 
- <div class="connexion">
-     <form method="POST">
-         <h1>Login</h1>
-         <div class="row">
-             <h4>username</h4>
-             <input type="username" name="username" required>
-         </div>
-         <div class="row">
-             <h4>Mot de passe</h4>
-             <input type="password" name="password" required>
-         </div>
-         <div class="row">
-             <button type="submit" name="connexion">connexion</button>
-             <a>Vous n'avez pas de compte ? créez vous en un !</a>
-         </div>
-     </form>
- </div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h1 class="card-title text-center">Login</h1>
+                        <form method="POST">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Mot de passe</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="connexion">Connexion</button>
+                            <a class="btn btn-link" href="inscription.php">Vous n'avez pas de compte ? Créez-vous en un !</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
