d64e76ee9415d16053420026d693088e8827a218
alexis fredriksen <alexis-fredriksen@semper-co.fr>
2023-06-12 02:26:11 -0700

    inscriptio connexion

---
+++ cocktail/src/page/connection.php
@@ -0,0 +1,19 @@
+ <?php include "../../config/connection.php" ?>
+
+ <div class="connexion">
+     <form method="POST">
+         <h1>Login</h1>
+         <div class="row">
+             <h4>username</h4>
+             <input type="username" name="username" required>
+         </div>
+         <div class="row">
+             <h4>Mot de passe</h4>
+             <input type="password" name="password" required>
+         </div>
+         <div class="row">
+             <button type="submit" name="connexion">connexion</button>
+             <a>Vous n'avez pas de compte ? créez vous en un !</a>
+         </div>
+     </form>
+ </div>
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+$dsn ='mysql:dbname=my_cocktail;localhost';
+$user = 'root';
+$password = 'root';
+
+$dbh = new PDO($dsn, $user, $password);
