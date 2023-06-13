1ca1d705efc8d545ed3b4ec30bb991132ce52365
Steelataure <alexandre0312@orange.fr>
2023-06-13 10:42:57 +0200

    liste des ingredients

--- cocktail/index.php
+++ cocktail/index.php
@@ -8,35 +8,37 @@ $stmt = $dbh->query($query);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Affichage des résultats
-if (count($results) > 0) {
-    ?>
-    <div class="table-responsive">
-        <table class="table table-striped">
-        <thead class="thead-dark">
-        <tr>
-            <th></th>
-            <th>Libellé</th>
-            <th>Image</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-
-    foreach ($results as $row) {
-        echo '<tr>';
-        echo '<td><input type="checkbox" name="checkbox[]" value="' . $row['id'] . '"></td>';
-        echo '<td>' . $row['Libelle'] . '</td>';
-        echo '<td><img src="' . $row['ImageID'] . '" alt="Image"></td>';
-        echo '</tr>';
-    }
-
-    echo '</tbody>';
-    echo '</table>';
-    echo '</div>';
-} else {
-    echo 'Aucun résultat trouvé.';
-}
-
+if (count($results) > 0) :
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-2 bg-light" style="height: 100vh;">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th></th>
+                            <th>Image</th>
+                            <th>Ingrédients</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($results as $row) : ?>
+                        <tr>
+                            <td><input type="checkbox" name="checkbox[]" value="<?= $row['id'] ?>" class="form-check-input mx-auto" style="width: 30px; height: 30px;"></td>
+                            <td><img src="/Cocktail-/public/assets/ingredients/image<?= $row['ImageID'] ?>.png" class="img-fluid" alt="Image" style="width: 100px; height: 100px; margin-left: 20px; margin-right: 20px;"></td>
+                            <td class="align-middle"><span class="d-block" style="font-size: 18px;"><?= $row['Libelle'] ?></span></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php else : ?>
+Aucun résultat trouvé.
+<?php endif;
 
 $content = ob_get_clean();
 include 'layout.php';
