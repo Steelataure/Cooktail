b66ad33ebb6c761e10aa784472c26eb772e739e5
SwannMartineau <swann.martineau@gmail.com>
2023-06-21 17:42:35 +0200

    fiche_recette (front à finir)

--- cocktail/cocktail_maker.php
+++ cocktail/cocktail_maker.php
@@ -4,12 +4,115 @@ session_start();
 
 $dbh = include '../config/config.php';
 
+
+$rootDir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
+$rootDir = basename(dirname($rootDir));
+// Affichage des résultats
+if (count($results) > 0) :
 ?>
 
+
 	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+
+						
+  
+
+							<header>
+								<h1>Nos Recettes phares.</h1>
+								<p>bla bla bla.</p>
+							</header>
+							<section class="tiles">
+                            
+                            <?php 
+							foreach ($results as $key => $row):
+							?>
+                            
+                            <article class="">
+                                <span class="image shadowCook2">
+								<div class="d-flex align-items-center position-relative ">
+										<img src="..<?= $row['Path'] ?>" alt="<?= $row['nom'] ?>" class="img-fluid" />
+										<!-- <img src="<?php echo DIRECTORY_SEPARATOR . $rootDir . DIRECTORY_SEPARATOR .  '/public/assets/cocktails/image' . $key . '.png'; ?>"/> -->
+								</div>
+
+                                </span>
+								<a href="./fiche_recette?id=<?= $row['cocktail'] ?>">
+                                    <h2><?= $row['CocktailLibelle'] ?></h2>
+                                    <div class="content">
+											<span>- <?= $row['Description'] ?></span>
+											<br></br>
+                                    </div>
+                                </a>
+                            </article>
+                            <?php endforeach; ?>
+                            <?php else : ?>
+                            Aucun résultat trouvé.
+                            <?php endif;?>
+
+                            
+								<!-- <article class="style1">
+									<span class="image">
+										<img src="./front/images/pic01.jpg" alt="" />
+									</span>
+									<a href="">
+										<h2>Magna</h2>
+										<div class="content">
+											<p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+										</div>
+									</a>
+								</article> -->
+
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2 class="shadowCookTxt">CONTACT</h2>
+								<form method="post" action="#">
+									<div class="fields">
+										<div class="field half">
+											<input type="text" name="name" id="name" placeholder="Name" />
+										</div>
+										<div class="field half">
+											<input type="email" name="email" id="email" placeholder="Email" />
+										</div>
+										<div class="field">
+											<textarea name="message" id="message" placeholder="Message"></textarea>
+										</div>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send" class="primary" /></li>
+									</ul>
+								</form>
+							</section>
+							<section>
+								<h2 class="shadowCookTxt">Réseaux</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon brands style2 fa-twitter shadowCookTxt"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-facebook-f shadowCookTxt"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-instagram shadowCookTxt"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon solid style2 fa-phone shadowCookTxt"><span class="label">Phone</span></a></li>
+									<li><a href="#" class="icon solid style2 fa-envelope shadowCookTxt"><span class="label">Email</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Projet Etudiant</li><li><a href=""> Paris</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
 	</body>
 
 <?php
 $content = ob_get_clean();
 include 'layout.php';
-?>
--- cocktail/fiche_recette.php
+++ cocktail/fiche_recette.php
@@ -26,6 +26,34 @@ $query->bindParam(":id", $CocktailId, PDO::PARAM_INT);
 $query->execute();
 $item = $query->fetchAll(PDO::FETCH_ASSOC);
 
+$sql2 = "SELECT *
+        FROM Cocktails, Ustensiles, Cocktails_Ustensiles, Files
+        WHERE Cocktails.id = Cocktails_Ustensiles.CocktailsID
+        AND CocktailsID = :id
+        AND Ustensiles.Ustensileid = Cocktails_Ustensiles.UstensilesID
+        AND Ustensiles.ImageID = Files.id";
+$query2 = $dbh->prepare($sql2);
+$query2->bindParam(":id", $CocktailId, PDO::PARAM_INT);
+$query2->execute();
+$item2 = $query2->fetchAll(PDO::FETCH_ASSOC);
+
+$sql3 = "SELECT *
+        FROM Cocktails, Ingredients, Cocktails_Ingredients, Files
+        WHERE Cocktails.id = Cocktails_Ingredients.CocktailID
+        AND CocktailID = :id
+        AND Ingredients.ID = Cocktails_Ingredients.Ingredientid
+        AND Ingredients.ImageID = Files.id";
+$query3 = $dbh->prepare($sql3);
+$query3->bindParam(":id", $CocktailId, PDO::PARAM_INT);
+$query3->execute();
+$item3 = $query3->fetchAll(PDO::FETCH_ASSOC);
+// $sql2 = "SELECT *
+//          FROM Ustensiles, Cocktails, Cocktails_Ustensiles
+//          WHERE "
+// $query = $dbh->prepare($sql);
+// $query->bindParam(":id", $CocktailId, PDO::PARAM_INT);
+// $query->execute();
+// $item = $query->fetchAll(PDO::FETCH_ASSOC);
 // Vérification si l'article existe
 if (count($item) == 0) {
     echo "Cocktail non trouvé.";
@@ -65,6 +93,10 @@ $rootDir = basename(dirname($rootDir));
         text-align: center;
     }
 
+    h3 {
+        text-align: center;
+    }
+
 </style>
 
 <div class="container">
@@ -74,11 +106,6 @@ $rootDir = basename(dirname($rootDir));
             <div class="card-body">
                 <h5 class="card-title"><?php echo $title; ?></h5>
             </div>
-            <?php
-            $i = 1;
-            while ($i < $etape) {
-                echo '<p>Etape <?php echo $i>';
-            }
             <p>Etape 1 : <?= $item[0]['Etape1'] ?></p>
             <p>Etape 2 : <?= $item[0]['Etape2'] ?></p>
             <p>Etape 3 : <?= $item[0]['Etape3'] ?></p>
@@ -91,7 +118,28 @@ $rootDir = basename(dirname($rootDir));
             <p>Etape 10 : <?= $item[0]['Etape10'] ?></p>
             <p>Etape 11 : <?= $item[0]['Etape11'] ?></p>
             <p>Etape 12 : <?= $item[0]['Etape12'] ?></p>
+            <h3>Ustensiles :</h3>
+           <?php 
+            foreach ($item2 as $row):
             ?>
+                <span class="image shadowCook2">
+                <div class="d-flex align-items-center position-relative ">
+                        <img src="..<?= $row['Path'] ?>" alt="<?= $row['nom'] ?>" class="img-fluid" />
+                        <!-- <img src="<?php echo DIRECTORY_SEPARATOR . $rootDir . DIRECTORY_SEPARATOR .  '/public/assets/cocktails/image' . $key . '.png'; ?>"/> -->
+                </div>
+                </span>
+            <?php endforeach; ?>
+            <h3>Ingredients :</h3>
+            <?php 
+            foreach ($item3 as $row):
+            ?>
+                <span class="image shadowCook2">
+                <div class="d-flex align-items-center position-relative ">
+                        <img src="..<?= $row['Path'] ?>" alt="<?= $row['nom'] ?>" class="img-fluid" />
+                        <!-- <img src="<?php echo DIRECTORY_SEPARATOR . $rootDir . DIRECTORY_SEPARATOR .  '/public/assets/cocktails/image' . $key . '.png'; ?>"/> -->
+                </div>
+                </span>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
