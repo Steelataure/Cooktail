97aac130b1f39e7808d7a15c045d4b8b05cbbf10
alexis fredriksen <alexis-fredriksen@semper-co.fr>
2023-06-22 11:32:48 -0700

    panier

--- cocktail/layout.php
+++ cocktail/layout.php
@@ -6,7 +6,7 @@
 
     <title>C O O K T A I L</title>
 
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
@@ -14,10 +14,12 @@
     <link rel="stylesheet" href="./front/assets/css/main.css" />
     <link rel="stylesheet" href="./front/assets/sass/wavy.scss" />
     <link rel="stylesheet" href="./front/assets/css/custom.css" />
-	<link rel="stylesheet" href="./front/assets/css/customCocktail.css">
+    <link rel="stylesheet" href="./front/assets/css/customCocktail.css">
 
     <!-- <link rel="stylesheet" href="./front/assets/css/bootstrap.min.css" /> -->
-    <noscript><link rel="stylesheet" href="./front/assets/css/noscript.css" /></noscript>
+    <noscript>
+        <link rel="stylesheet" href="./front/assets/css/noscript.css" />
+    </noscript>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="./front/images/Cooktail_Icone_Lumiere.ico">
 </head>
@@ -26,11 +28,11 @@
 
 
     <header id="header" class="row">
-            
+
         <!-- Header -->
         <div class="inner titreLogo col-5">
-        <!-- Logo -->
-        <div class="bubbles iconeCoock" style="max-height:50px !important; font-size: 5px;">
+            <!-- Logo -->
+            <div class="bubbles iconeCoock" style="max-height:50px !important; font-size: 5px;">
                 <i class="fas fa-circle shadowCookTxt bubble b2"></i>
                 <i class="fas fa-circle shadowCookTxt bubble b5"></i>
                 <i class="fas fa-circle shadowCookTxt bubble"></i>
@@ -38,60 +40,60 @@
                 <i class="fas fa-circle shadowCookTxt bubble b3"></i>
                 <i class="fas fa-circle shadowCookTxt bubble b7"></i>
                 <i class="fas fa-circle shadowCookTxt bubble b8"></i>
-				</div>
+            </div>
             <a href="index" class="logo">
 
-            <h4 class="borderWave iconeCoock"><i class="fas fa-cocktail" style="font-size "></i>Cooktail</h4>
-            <h4 class="waveTitle iconeCoock"><i class="fas fa-cocktail" style="font-size "></i>Cooktail</h4>
+                <h4 class="borderWave iconeCoock"><i class="fas fa-cocktail" style="font-size "></i>Cooktail</h4>
+                <h4 class="waveTitle iconeCoock"><i class="fas fa-cocktail" style="font-size "></i>Cooktail</h4>
 
             </a>
         </div>
         <div class=" col-5">
             <div class="row iconeMenu">
-                        <?php if (isset($_SESSION['isLogged'])) { ?>
-                            
-                           
-                            
-                            <li class="nav-item">
-                                <!-- <span>Connecté en tant que : <?php echo $_SESSION['isLogged']; ?></span> -->
-                                <form method="POST" action="logout">
-                                    <button type="submit" name="deconnexion">Déconnexion</button>
-                                </form>
-                            </li>
-                            
-                            <li class="">
-                                <a class="nav-link userBar" href="login"><i class="fas fa-shopping-basket shadowCookTxt "></i></a>
-                            </li>
-                        <?php } else { ?>
-                            <li class="">
-                                <a class="nav-link userBar" href="inscription"><i class="fas fa-user-plus shadowCookTxt"></i></a>
-                            </li>
-                            
-                            <li class="">
-                                <a class="nav-link userBar" href="login"><i class="fas fa-user shadowCookTxt"></i></a>
-                            </li>
-                        <?php } ?>
-                        
-                            <li class="">
-                                <a class="nav-link userBar" href="#menu"><i class="fas fa-bars shadowCookTxt "></i></a>
-                            </li>
+                <?php if (isset($_SESSION['isLogged'])) { ?>
+
+
+
+                <li class="nav-item">
+                    <!-- <span>Connecté en tant que : <?php echo $_SESSION['isLogged']; ?></span> -->
+                    <form method="POST" action="logout">
+                        <button type="submit" name="deconnexion">Déconnexion</button>
+                    </form>
+                </li>
+
+                <li class="">
+                    <a class="nav-link userBar" href="login"><i class="fas fa-shopping-basket shadowCookTxt "></i></a>
+                </li>
+                <?php } else { ?>
+                <li class="">
+                    <a class="nav-link userBar" href="inscription"><i class="fas fa-user-plus shadowCookTxt"></i></a>
+                </li>
+
+                <li class="">
+                    <a class="nav-link userBar" href="login"><i class="fas fa-user shadowCookTxt"></i></a>
+                </li>
+                <?php } ?>
+
+                <li class="">
+                    <a class="nav-link userBar" href="#menu"><i class="fas fa-bars shadowCookTxt "></i></a>
+                </li>
             </div>
-            
+
         </div>
 
     </header>
 
-  <!-- Menu -->
+    <!-- Menu -->
     <nav id="menu">
-      <h2>Menu</h2>
-      <ul>
-        <li><a href="index"><i class="mr-4 fas fa-home"></i>Accueil</a></li>
-        <li><a href="shop"><i class="mr-4 fas fa-shopping-cart"></i>Boutique</a></li>
-        <li><a href="vote"><i class="mr-4 fas fa-users"></i>Communauté</a></li>
-        <li><a href="create"><i class="mr-4 fas fa-plus"></i>Ajoute ton cocktail</a></li>
-        <li><a href="cocktail_maker"><i class="mr-4 fas fa-wine-glass-alt"></i>Cocktail Maker</a></li>
-
-      </ul>
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="index"><i class="mr-4 fas fa-home"></i>Accueil</a></li>
+            <li><a href="shop"><i class="mr-4 fas fa-shopping-cart"></i>Boutique</a></li>
+            <li><a href="vote"><i class="mr-4 fas fa-users"></i>Communauté</a></li>
+            <li><a href="create"><i class="mr-4 fas fa-plus"></i>Ajoute ton cocktail</a></li>
+            <li><a href="cocktail_maker"><i class="mr-4 fas fa-wine-glass-alt"></i>Cocktail Maker</a></li>
+            <li><a href="panier"><i class="mr-4 fas fa-shopping-cart"></i>Panier</a></li>
+        </ul>
 
     </nav>
 
@@ -161,5 +163,5 @@
 <script src="./front/assets/js/util.js"></script>
 <script src="./front/assets/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
