37269c3f54d70a2e6a7dcc1f89b6d863ee2e4be7
alexis fredriksen <alexis-fredriksen@semper-co.fr>
2023-06-23 00:29:39 -0700

    Rdirection ajouter cocktail

--- cocktail/cocktail_maker.php
+++ cocktail/cocktail_maker.php
@@ -47,6 +47,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           $stmtCocktails->execute();
 
           echo "Le cocktail a été ajouté avec succès.";
+          if(isset($_POST['isClassic'])){
+            header("Location: shop");
+            exit();
+          }else{
+            header("Location: vote");
+            exit();
+          }
       } else {
           echo "Une erreur s'est produite lors de l'upload de l'image.";
       }
@@ -74,157 +81,167 @@ if (isset($_SESSION['userID'])) {
 
 <body>
 
-<div id="wrapper">
+    <div id="wrapper">
 
-				<!-- Main -->
-					<div id="main">
-						<div class="innerMaker">
-							<!-- <header>
+        <!-- Main -->
+        <div id="main">
+            <div class="innerMaker">
+                <!-- <header>
 								<h1>Notre Cocktail Maker</h1>
 								<p>bla bla bla.</p>
 							</header> -->
 
-              <div class="row fontCocktail">
+                <div class="row fontCocktail">
 
-            
-                <!-- CARD BLOC 1 -->
-                  <div class="col-4 card loginWave loginWaveInsc shadowCook formMaker">
-                    <div class="card-body blocIng">
-                        <h1 class="card-title text-center">Ingrédients</h1>
-                        <form method="POST">
-                              
-                              <?php foreach ($results as $row) { ?>
+
+                    <!-- CARD BLOC 1 -->
+                    <div class="col-4 card loginWave loginWaveInsc shadowCook formMaker">
+                        <div class="card-body blocIng">
+                            <h1 class="card-title text-center">Ingrédients</h1>
+                            <form method="POST">
+
+                                <?php foreach ($results as $row) { ?>
                                 <div class="row">
-                                  <div  class="col-5" >
-                                  <input type="checkbox" name="myCheckbox<?php echo $row['id']?>" onclick="myFunction(<?php echo $row['id']; ?>)" id="<?php echo $row['id']; ?>" value="<?php echo $row['couleur']; ?>">
-                                  <label for="<?php echo $row['id']; ?>"><?php echo $row['Libelle']; ?> 
-                                  </div> 
-                                  
-                                  <div class="col-7 padd row displayQTT ml-1" id="display<?php echo $row['id']; ?>">
-                                  <span class="col-3 padd">Quantité :</span>
-                                  <input type="text" name="myQuant<?php echo $row['id']?>" class="col-6 inputQTT" id="QTT<?php echo $row['id']; ?>">
-                                  </div>
+                                    <div class="col-5">
+                                        <input type="checkbox" name="myCheckbox<?php echo $row['id']?>"
+                                            onclick="myFunction(<?php echo $row['id']; ?>)"
+                                            id="<?php echo $row['id']; ?>" value="<?php echo $row['couleur']; ?>">
+                                        <label for="<?php echo $row['id']; ?>"><?php echo $row['Libelle']; ?>
+                                    </div>
+
+                                    <div class="col-7 padd row displayQTT ml-1" id="display<?php echo $row['id']; ?>">
+                                        <span class="col-3 padd">Quantité :</span>
+                                        <input type="text" name="myQuant<?php echo $row['id']?>" class="col-6 inputQTT"
+                                            id="QTT<?php echo $row['id']; ?>">
+                                    </div>
                                 </div>
-                                
-                              </label><br>
-                              
-                              <script>
-                              function myFunction(id) {
-                                var checkBox = document.getElementById(id);
-                                var text = document.getElementById("display"+id);
-                                if (checkBox.checked == true){
-                                  text.style.display = "inherit";
-                                } else {
-                                  text.style.display = "none";
+
+                                </label><br>
+
+                                <script>
+                                function myFunction(id) {
+                                    var checkBox = document.getElementById(id);
+                                    var text = document.getElementById("display" + id);
+                                    if (checkBox.checked == true) {
+                                        text.style.display = "inherit";
+                                    } else {
+                                        text.style.display = "none";
+                                    }
                                 }
-                              }
 
-                              document.addEventListener("DOMContentLoaded", function (event) {
-                                let check = 'input[name=myCheckbox'+<?php echo $row['id']?>+']';
-                                  var _selector = document.querySelector(check);
-                                  let quant = 'input[name="myQuant<?php echo $row['id'] ?>"]';
-                                  var quantites = document.querySelector(quant);
-                                  _selector.addEventListener('change', function (event2) {
-                                    
-                                      if (_selector.checked) {
-                                        var id = '<?php echo $row['Libelle']?>';
-                                        var value = _selector.value;
-                                        
-                                        var ingredient =  {
-                                            text: id,
-                                            color: value
-                                          };
-                                        ingredient.quantite="";
+                                document.addEventListener("DOMContentLoaded", function(event) {
+                                    let check = 'input[name=myCheckbox' + <?php echo $row['id']?> + ']';
+                                    var _selector = document.querySelector(check);
+                                    let quant = 'input[name="myQuant<?php echo $row['id'] ?>"]';
+                                    var quantites = document.querySelector(quant);
+                                    _selector.addEventListener('change', function(event2) {
 
-                                        drink.push(ingredient);
-                                        getDrink();
-                                        quantites.addEventListener('change', function(event3) {
-                                          ingredient.quantite = quantites.value;
-                                          getDrink();
-                                        });
-                                      } else {
-                                        drink.splice(drink.findIndex(v => v.text === '<?php echo $row['Libelle']?>'), 1);
-                                        console.log(drink)
-                                        getDrink();
-                                      }
-                                  });
-                              });
+                                        if (_selector.checked) {
+                                            var id = '<?php echo $row['Libelle']?>';
+                                            var value = _selector.value;
 
-                              </script>
+                                            var ingredient = {
+                                                text: id,
+                                                color: value
+                                            };
+                                            ingredient.quantite = "";
 
-                              <?php } ?>
-                                                
-                        </form>
+                                            drink.push(ingredient);
+                                            getDrink();
+                                            quantites.addEventListener('change', function(event3) {
+                                                ingredient.quantite = quantites.value;
+                                                getDrink();
+                                            });
+                                        } else {
+                                            drink.splice(drink.findIndex(v => v.text ===
+                                                '<?php echo $row['Libelle']?>'), 1);
+                                            console.log(drink)
+                                            getDrink();
+                                        }
+                                    });
+                                });
+                                </script>
+
+                                <?php } ?>
+
+                            </form>
+                        </div>
                     </div>
-                </div>
 
-                <!-- COCKTAIL MAKER /!\ -->
-                <div class="col-4 mt-5 padd"> 
-                  <main class="page__content mt-5">
-                    <section class="cocktail">
-                      <h2 class="cocktail__label" id="drink-name"></h2>
-                      <div class="glass" id="glass">
-                        <div class="garnish"></div>
-                        <div class="bowl">
-                          <div class="ingredient-colors" id="ingredient-colors"></div>
-                          <ul class="ingredient-list" id="ingredient-list"></ul>
-                        </div>
-                        <div class="stem"></div>
-                        <div class="base"></div>
-                      </div>
-                    </section>
-                  </main>
-                </div>
-
-
-                <div class="col-4 card loginWave loginWaveInsc shadowCook formMaker">
-                    <div class="card-body blocIng">
-                        <h1 class="card-title text-center">Informations</h1>
-                        <form method="POST" enctype="multipart/form-data">
-
-                        <div class="form-group">
-                            <label for="nomCocktail">Nom du cocktail</label>
-                            <input type="text" class=" inputQTT" id="nomCocktail" name="nomCocktail" required>
-
-                        </div>
-
-                        <div class="form-group">
-                            <label for="description">Description du cocktail</label>
-                        </div>
-                        <textarea class="form-control" id="description" name="description" rows="6" required></textarea>
-
-                        <div class="form-group my-4">
-                            <label for="image">Image du cocktail</label>
-                        </div>
-                        <input type="file" class="form-control-file mx-5 mb-5" id="image" name="image_cocktail" required>
-
-                        <?php if ($isUserAdmin) : ?>
-                            <div class="form-group">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="isClassic" name="isClassic">
-                                    <label class="form-check-label" for="isClassic">Mettre en magasin</label>
+                    <!-- COCKTAIL MAKER /!\ -->
+                    <div class="col-4 mt-5 padd">
+                        <main class="page__content mt-5">
+                            <section class="cocktail">
+                                <h2 class="cocktail__label" id="drink-name"></h2>
+                                <div class="glass" id="glass">
+                                    <div class="garnish"></div>
+                                    <div class="bowl">
+                                        <div class="ingredient-colors" id="ingredient-colors"></div>
+                                        <ul class="ingredient-list" id="ingredient-list"></ul>
+                                    </div>
+                                    <div class="stem"></div>
+                                    <div class="base"></div>
                                 </div>
-                            </div>
-                        <?php endif; ?>
-
-                        <button type="submit" id="submit_create" class="primary " name="ajouter" style="margin-left: 30% !important;">Créer mon cocktail</button>
-                        </form>
+                            </section>
+                        </main>
                     </div>
+
+
+                    <div class="col-4 card loginWave loginWaveInsc shadowCook formMaker">
+                        <div class="card-body blocIng">
+                            <h1 class="card-title text-center">Informations</h1>
+                            <form method="POST" enctype="multipart/form-data">
+
+                                <div class="form-group">
+                                    <label for="nomCocktail">Nom du cocktail</label>
+                                    <input type="text" class=" inputQTT" id="nomCocktail" name="nomCocktail" required>
+
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="description">Description du cocktail</label>
+                                </div>
+                                <textarea class="form-control" id="description" name="description" rows="6"
+                                    required></textarea>
+
+                                <div class="form-group my-4">
+                                    <label for="image">Image du cocktail</label>
+                                </div>
+                                <input type="file" class="form-control-file mx-5 mb-5" id="image" name="image_cocktail"
+                                    required>
+
+                                <?php if ($isUserAdmin) : ?>
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="isClassic" name="isClassic">
+                                        <!-- <form method="">
+
+                                        </form> -->
+                                        <label class="form-check-label" for="isClassic">Mettre en
+                                            magasin</label>
+                                    </div>
+                                </div>
+                                <?php endif; ?>
+
+                                <button type="submit" id="submit_create" class="primary " name="ajouter"
+                                    style="margin-left: 30% !important;">Créer mon cocktail</button>
+                            </form>
+                        </div>
+                    </div>
+
                 </div>
-
-              </div>
             </div>
-          </div>
-</div>
+        </div>
+    </div>
 
-<!-- partial:index.partial.html -->
-  <script>
+    <!-- partial:index.partial.html -->
+    <script>
     var drink = [];
     var quantite = "";
-  </script>
-<!-- partial -->
-  <script src='https://cdn.jsdelivr.net/npm/zdog'></script>
-  <script  src="./front/assets/js/customCocktail.js"></script>
+    </script>
+    <!-- partial -->
+    <script src='https://cdn.jsdelivr.net/npm/zdog'></script>
+    <script src="./front/assets/js/customCocktail.js"></script>
 </body>
 
 <?php
