e51f8f48cda21469e1a3f71614a27c8a2ad34616
unknown <senecalpaul@outlook.fr>
2023-06-23 09:38:43 +0200

    add detail

--- cocktail/footer.php
+++ cocktail/footer.php
@@ -11,11 +11,11 @@
 							<section>
 								<h2 class="shadowCookTxt">Réseaux</h2>
 								<ul class="icons">
-									<li><a href="#" class="icon brands style2 fa-twitter shadowCookTxt"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-facebook-f shadowCookTxt"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-instagram shadowCookTxt"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon solid style2 fa-phone shadowCookTxt"><span class="label">Phone</span></a></li>
-									<li><a href="#" class="icon solid style2 fa-envelope shadowCookTxt"><span class="label">Email</span></a></li>
+									<li><a href="https://twitter.com/?lang=fr" class="icon brands style2 fa-twitter shadowCookTxt"><span class="label">Twitter</span></a></li>
+									<li><a href="https://fr-fr.facebook.com/" class="icon brands style2 fa-facebook-f shadowCookTxt"><span class="label">Facebook</span></a></li>
+									<li><a href="https://www.instagram.com/" class="icon brands style2 fa-instagram shadowCookTxt"><span class="label">Instagram</span></a></li>
+									<li><a href="tel:+337000000" class="icon solid style2 fa-phone shadowCookTxt"><span class="label">Phone</span></a></li>
+									<li><a href="mailto:xanto78@live.fr" class="icon solid style2 fa-envelope shadowCookTxt"><span class="label">Email</span></a></li>
 								</ul>
 							</section>
 							<ul class="copyright">
